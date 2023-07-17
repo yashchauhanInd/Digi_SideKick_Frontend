089d6c9eb01a6e6d86bb4b637d4861c3ad773afa
yashchauhanInd <yashchauhan.ind@gmail.com>
2023-07-17 20:17:08 +0530

    cd frontend npm start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('/users')
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers([...users, data]);
+        setName('');
+        setEmail('');
+        setError(null);
+      })
+      .catch((error) => {
+        setError('Error creating user');
+      });
+  };
+
+  const handleDelete = (userId) => {
+    fetch(`/users/${userId}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        setUsers(users.filter((user) => user._id !== userId));
+        setError(null);
+      })
+      .catch((error) => {
+        setError('Error deleting user');
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Users</h1>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button type="submit">Create User</button>
+      </form>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            {user.name} - {user.email}
+            <button onClick={() => handleDelete(user._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
